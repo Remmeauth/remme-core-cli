459d095586622fadaedf42abcbf2760b6842dfc9
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2019-04-09 15:38:59 +0300

    Create skeleton of the project (#1)

---
+++ cli/entrypoint.py
@@ -0,0 +1,14 @@
+"""
+Provide implementation of the command line interface to interact with Remme-core.
+"""
+import click
+
+
+@click.group()
+@click.version_option()
+@click.help_option()
+def cli():
+    """
+    Command-line interface to interact with Remme-core.
+    """
+    pass
