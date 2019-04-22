a8cef2ae1bd724aa2b3aebc8d4c79c759e9a6098
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2019-04-22 14:20:10 +0300

    Solve weekly technical debt (#13)

--- cli/constants.py
+++ cli/constants.py
@@ -1,10 +1,14 @@
 """
 Provide constants for command line interface.
 """
-ADDRESS_REGEXP = '[0-9a-f]{70}'
+ADDRESS_REGEXP = r'^[0-9a-f]{70}$'
+DOMAIN_NAME_REGEXP = r'(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]'
 
-FAILED_EXIT_FROM_COMMAND = -1
+PASSED_EXIT_FROM_COMMAND_CODE = 0
+FAILED_EXIT_FROM_COMMAND_CODE = -1
 
 NODE_URL_ARGUMENT_HELP_MESSAGE = 'Apply the command to the specified node by its URL.'
 
 CLI_CONFIG_FILE_NAME = 'remme-core-cli'
+
+NODE_IP_ADDRESS_FOR_TESTING = '159.89.104.9'
