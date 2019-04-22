3cf9c522de4e5564ac7cc4c0e5a11b0e636ceb53
Anastasiia Bilova <anastasiabilova@gmail.com>
2019-04-22 20:26:04 +0300

    Create tests and add validation forms

--- cli/public_key/cli.py
+++ cli/public_key/cli.py
@@ -1,21 +1,24 @@
 """
 Provide implementation of the command line interface's public key commands.
 """
-import re
+import asyncio
 import sys
 
-import asyncio
 import click
 from remme import Remme
 
 from cli.constants import (
-    ADDRESS_REGEXP,
-    FAILED_EXIT_FROM_COMMAND,
+    FAILED_EXIT_FROM_COMMAND_CODE,
     NODE_URL_ARGUMENT_HELP_MESSAGE,
 )
+from cli.public_key.forms import GetPublicKeysForm
 from cli.public_key.help import GET_PUBLIC_KEYS_ADDRESS_ARGUMENT_HELP_MESSAGE
 from cli.public_key.service import PublicKey
-from cli.utils import dict_to_pretty_json
+from cli.utils import (
+    default_node_url,
+    print_errors,
+    print_result,
+)
 
 loop = asyncio.get_event_loop()
 
@@ -29,23 +32,29 @@ def public_key_commands():
 
 
 @click.option('--address', type=str, required=True, help=GET_PUBLIC_KEYS_ADDRESS_ARGUMENT_HELP_MESSAGE)
-@click.option('--node-url', type=str, required=False, help=NODE_URL_ARGUMENT_HELP_MESSAGE)
+@click.option('--node-url', type=str, required=False, help=NODE_URL_ARGUMENT_HELP_MESSAGE, default=default_node_url())
 @public_key_commands.command('get-list')
 def get_public_keys(address, node_url):
     """
     Get list of the public keys by account address.
     """
-    if re.match(pattern=ADDRESS_REGEXP, string=address) is None:
-        click.echo(f'The following address `{address}` is not valid.')
-        sys.exit(FAILED_EXIT_FROM_COMMAND)
+    arguments, errors = GetPublicKeysForm().load({
+        'address': address,
+        'node_url': node_url,
+    })
 
-    if node_url is None:
-        node_url = 'localhost'
+    if errors:
+        print_errors(errors)
+        sys.exit(FAILED_EXIT_FROM_COMMAND_CODE)
+
+    address = arguments.get('address')
+    node_url = arguments.get('node_url')
 
     remme = Remme(network_config={
         'node_address': str(node_url) + ':8080',
     })
 
-    addresses = loop.run_until_complete(PublicKey(service=remme).get_list(address=address))
+    public_key_service = PublicKey(service=remme)
+    addresses = loop.run_until_complete(public_key_service.get_list(address=address))
 
-    click.echo(dict_to_pretty_json(data=addresses))
+    print_result(result=addresses)
---
+++ tests/public_key/test_public_key.py
@@ -0,0 +1,161 @@
+"""
+Provide tests for command line interface's public key commands.
+"""
+import json
+import re
+
+from click.testing import CliRunner
+
+from cli.constants import (
+    ADDRESS_REGEXP,
+    FAILED_EXIT_FROM_COMMAND_CODE,
+    NODE_IP_ADDRESS_FOR_TESTING,
+    PASSED_EXIT_FROM_COMMAND_CODE,
+)
+from cli.entrypoint import cli
+from cli.utils import (
+    dict_to_pretty_json,
+    return_async_value,
+)
+
+ADDRESS_PRESENTED_ON_THE_TEST_NODE = '1120076ecf036e857f42129b58303bcf1e03723764a1702cbe98529802aad8514ee3cf'
+
+
+def test_get_public_keys():
+    """
+    Case: get a list of the public keys by account address.
+    Expect: list of public keys, each public key matched regexp checking.
+    """
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        'public-key',
+        'get-list',
+        '--address',
+        ADDRESS_PRESENTED_ON_THE_TEST_NODE,
+        '--node-url',
+        NODE_IP_ADDRESS_FOR_TESTING,
+    ])
+
+    assert PASSED_EXIT_FROM_COMMAND_CODE == result.exit_code
+
+    for public_key in json.loads(result.output):
+        assert re.match(pattern=ADDRESS_REGEXP, string=public_key) is not None
+
+
+def test_get_public_keys_invalid_address():
+    """
+    Case: get a list of the public keys by invalid address.
+    Expect: the following address is not valid error message.
+    """
+    invalid_address = '1120076ecf036e857f42129b58303bcf1e03723764a1702cbe98529802aad8514ee3zz'
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        'public-key', 'get-list', '--address', invalid_address, '--node-url', NODE_IP_ADDRESS_FOR_TESTING,
+    ])
+
+    assert FAILED_EXIT_FROM_COMMAND_CODE == result.exit_code
+    assert f'The following address `{invalid_address}` is invalid.' in result.output
+
+
+def test_get_public_keys_without_node_url(mocker):
+    """
+    Case: get a list of the public keys by address without passing node URL.
+    Expect: list of public keys is returned from node on localhost.
+    """
+    expected_list_of_public_keys = [
+        'a23be14785e7b073b50e24f72e086675289795b969a895a7f02202404086946e8ddc5b',
+        'a23be17265e8393dd9ae7a46f1be662f86130c434fd54576a7d92b678e5c30de4f677f',
+    ]
+
+    mock_get_list = mocker.patch('cli.public_key.service.PublicKey.get_list')
+    mock_get_list.return_value = return_async_value(expected_list_of_public_keys)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, ['public-key', 'get-list', '--address', ADDRESS_PRESENTED_ON_THE_TEST_NODE])
+
+    assert PASSED_EXIT_FROM_COMMAND_CODE == result.exit_code
+
+    for public_key in json.loads(result.output):
+        assert re.match(pattern=ADDRESS_REGEXP, string=public_key) is not None
+
+
+def test_get_public_keys_invalid_node_url():
+    """
+    Case: get a list of the public keys by passing invalid node URL.
+    Expect: the following node URL is invalid error message.
+    """
+    invalid_node_url = 'domainwithoutextention'
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        'public-key',
+        'get-list',
+        '--address',
+        ADDRESS_PRESENTED_ON_THE_TEST_NODE,
+        '--node-url',
+        invalid_node_url,
+    ])
+
+    expected_error = {
+        'node_url': [
+            f'The following node URL `{invalid_node_url}` is invalid.',
+        ],
+    }
+
+    assert FAILED_EXIT_FROM_COMMAND_CODE == result.exit_code
+    assert dict_to_pretty_json(expected_error) in result.output
+
+
+def test_get_public_keys_node_url_with_http():
+    """
+    Case: get a list of the public keys by passing node URL with explicit HTTP protocol.
+    Expect: the following node URL contains protocol error message.
+    """
+    node_url_with_http_protocol = 'http://masternode.com'
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        'public-key',
+        'get-list',
+        '--address',
+        ADDRESS_PRESENTED_ON_THE_TEST_NODE,
+        '--node-url',
+        node_url_with_http_protocol,
+    ])
+
+    expected_error = {
+        'node_url': [
+            f'Pass the following node URL `{node_url_with_http_protocol}` without protocol (http, https, etc.).',
+        ],
+    }
+
+    assert FAILED_EXIT_FROM_COMMAND_CODE == result.exit_code
+    assert dict_to_pretty_json(expected_error) in result.output
+
+
+def test_get_public_keys_node_url_with_https():
+    """
+    Case: get a list of the public keys by passing node URL with explicit HTTPS protocol.
+    Expect: the following node URL contains protocol error message.
+    """
+    node_url_with_https_protocol = 'https://masternode.com'
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        'public-key',
+        'get-list',
+        '--address',
+        ADDRESS_PRESENTED_ON_THE_TEST_NODE,
+        '--node-url',
+        node_url_with_https_protocol,
+    ])
+
+    expected_error = {
+        'node_url': [
+            f'Pass the following node URL `{node_url_with_https_protocol}` without protocol (http, https, etc.).',
+        ],
+    }
+
+    assert FAILED_EXIT_FROM_COMMAND_CODE == result.exit_code
+    assert dict_to_pretty_json(expected_error) in result.output
