3f32af4e8aa9516c33118269fee3ccfc5da075f4
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2019-05-02 13:43:01 +0300

    Remove inits from tests folder, sort requirements

--- tests/constants.py
+++ tests/constants.py
@@ -1,7 +1,3 @@
 """
 Provide constants for testing a command line interface.
 """
-import pathlib
-
-NODE_PRIVATE_KEY_DIRECTORY_PATH = str(pathlib.Path.home()) + '/docker/volumes/remme_validator_keys/_data/'
-NODE_PRIVATE_KEY_FILE_PATH_IN_TESTING = NODE_PRIVATE_KEY_DIRECTORY_PATH + 'validator.priv'
