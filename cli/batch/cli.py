2f79f460bab2481ba0318ee66fd724b9b46ecc8e
Art17 <ASaprykin97@gmail.com>
2019-05-14 16:03:44 +0300

    REM-1296: Get list of the batches (#12)
    * REM-1296: Get list of the batches

--- cli/batch/cli.py
+++ cli/batch/cli.py
@@ -7,11 +7,17 @@ import click
 from remme import Remme
 
 from cli.batch.forms import (
+    GetBatchesListForm,
     GetBatchForm,
     GetBatchStatusForm,
 )
 from cli.batch.help import (
+    BATCH_HEAD_ARGUMENT_HELP_MESSAGE,
     BATCH_ID_ARGUMENT_HELP_MESSAGE,
+    BATCH_IDENTIFIERS_ARGUMENT_HELP_MESSAGE,
+    BATCH_LIMIT_ARGUMENT_HELP_MESSAGE,
+    BATCH_REVERSE_ARGUMENT_HELP_MESSAGE,
+    BATCH_START_ARGUMENT_HELP_MESSAGE,
     BATCH_STATUS_IDENTIFIER_ARGUMENT_HELP_MESSAGE,
 )
 from cli.batch.service import Batch
@@ -90,6 +96,55 @@ def get_batch_status(id, node_url):
     })
 
     result, errors = Batch(service=remme).get_status(id=batch_id)
+
+    if errors is not None:
+        print_errors(errors=errors)
+        sys.exit(FAILED_EXIT_FROM_COMMAND_CODE)
+
+    print_result(result=result)
+
+
+@click.option('--ids', required=False, type=str, help=BATCH_IDENTIFIERS_ARGUMENT_HELP_MESSAGE)
+@click.option('--start', required=False, type=str, help=BATCH_START_ARGUMENT_HELP_MESSAGE)
+@click.option('--limit', required=False, type=int, help=BATCH_LIMIT_ARGUMENT_HELP_MESSAGE)
+@click.option('--head', required=False, type=str, help=BATCH_HEAD_ARGUMENT_HELP_MESSAGE)
+@click.option('--reverse', required=False, is_flag=True, help=BATCH_REVERSE_ARGUMENT_HELP_MESSAGE)
+@click.option('--node-url', required=False, type=str, help=NODE_URL_ARGUMENT_HELP_MESSAGE, default=default_node_url())
+@batch_commands.command('get-list')
+def get_batches(ids, start, limit, head, reverse, node_url):
+    """
+    Get a list of batches.
+    """
+    arguments, errors = GetBatchesListForm().load({
+        'ids': ids,
+        'start': start,
+        'limit': limit,
+        'head': head,
+        'reverse': reverse,
+        'node_url': node_url,
+    })
+
+    if errors:
+        print_errors(errors=errors)
+        sys.exit(FAILED_EXIT_FROM_COMMAND_CODE)
+
+    batch_ids = arguments.get('ids')
+    start = arguments.get('start')
+    limit = arguments.get('limit')
+    head = arguments.get('head')
+    node_url = arguments.get('node_url')
+
+    remme = Remme(network_config={
+        'node_address': str(node_url) + ':8080',
+    })
+
+    result, errors = Batch(service=remme).get_list(
+        ids=batch_ids,
+        start=start,
+        limit=limit,
+        head=head,
+        reverse=reverse,
+    )
 
     if errors is not None:
         print_errors(errors=errors)
